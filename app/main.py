b17df9179fdf437731874e7cfbaa80c09cc04af5
Seungpyo Suh <me@sspzoa.io>
2024-09-30 21:48:53 +0900

    feat: dot env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 import os
+from pathlib import Path
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from openai import OpenAI
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / ".env"
+
+load_dotenv(ENV_FILE)
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
